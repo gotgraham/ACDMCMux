831dc9712ca87a5d3aecfa0187fb130803eebe3c
Jacob Graham <jake@shortstacklab.com>
2019-12-27 13:30:35 -0800

    Collect more data from the controller

--- ACDMCMux.ino
+++ ACDMCMux.ino
@@ -32,6 +32,7 @@ struct DCDCStatus {
 struct ControllerStatus {
   uint32 temps[6];
   uint32 current[4];
+  uint32 phi_int;
 } controller_status;
 
 #pragma pack(pop)   // restore original alignment from stack
@@ -72,7 +73,7 @@ void dcDcCheckStatus(void) {
     if (canId == 0x1d6) {
       dc_dc_status.temp1 = (cToF(buf[3] - 40) * 100);
       dc_dc_status.temp2 = (cToF(buf[4] - 40) * 100);
-      dc_dc_status.voltage = (buf[2] / 12.7 * 100);
+      dc_dc_status.voltage = (buf[2] / 12.7) * 100;
       dc_dc_status.current = (buf[5] * 100);
     }
   }
@@ -85,45 +86,58 @@ void dcDcCheckStatus(void) {
 // Controller
 //
 
+void serialFlush(){
+  byte w = 0;
+
+   for (int i = 0; i < 10; i++)
+   {
+     while (Serial.available() > 0)
+     {
+       char k = Serial.read();
+       w++;
+       delay(1);
+     }
+     delay(1);
+   }
+}   
+
 uint16 readControllerValue(char request)
 {
   uint16 result = 0;   
   uint16 masked_result = 0; 
+  
+  Serial2.write('*');         // Send stop character
+  delay(10);                  // Wait for it to be processed
     
-  while(Serial2.read() >= 0); // Depleat the receive buffer
+  serialFlush();
 
-  do {
-    Serial2.write(request); 
+  static char inBuff[2]; // Read a buffer
     
-    // Request value    
-    if (Serial2.available() > 0) {      
-      char inBuff[3]; // Read a buffer
-      Serial2.readBytes(inBuff, sizeof(inBuff));
-      result = ntohs(*((uint16*)&inBuff[0]));        
-    } 
-    delay(10);
+  Serial2.write(request);       
+  Serial2.readBytes(inBuff, sizeof(inBuff));
 
-    masked_result = result & 0xF000;
-    
-  } while(masked_result != 0x8000 && masked_result != 0x7000 && masked_result != 0x6000);
- 
-  Serial2.write('*'); // Stop value
+  result = ntohs(*((uint16*)&inBuff[0]));  
+      
+  Serial2.write('*'); // Send Stop Char
  
   return result;
 }
 
 void storeTemp(uint8 which, uint16 raw_value) {
   float temp = ((float)(raw_value & 0x7FFF) / 2);
-  if (temp > 0 && temp < 150) {
-    controller_status.temps[which] = (temp * 100);
-  }
+  controller_status.temps[which] = (cToF(temp) * 100);
 }
 
 void storeCurrent(uint8 which, uint16 raw_value) {
-  float current = raw_value >= 0x8000 ? (float)(raw_value & 0x7FFF) : 0;//(float)(raw_value - 0x8000);
-  current /= 10;
-  
-  controller_status.current[which] = (current * 100);
+  // Raw value - 0 point (0x8000)
+  // float current = raw_value - 0x800;
+
+  // Divide by Clarke Factor (1.5)
+  // Divide by Scaling Factor (16)
+  // Divide by 10-bit ADC (1024)
+  // Multiply by 5v (since 0-5v range)
+  // Divide by mV/A for current sensor  
+  controller_status.current[which] = raw_value; //current / 16 / 1.5 / 1024 * 5 / 0.0012;
 }
 
 void controllerCheckStatus() {
@@ -135,13 +149,18 @@ void controllerCheckStatus() {
   else {
     ++current_temp_sensor;
   }
+  storeCurrent(0, readControllerValue('c')); // Phase Current, Filtered
+  storeCurrent(1, readControllerValue('d')); // Phase Current, Requested
+  storeCurrent(2, readControllerValue('t')); // Max Phase Current
+  storeCurrent(3, readControllerValue('u')); // Field Weakening
+  controller_status.phi_int = readControllerValue('b'); // Phi Int
 }
 
 void fanHandler() {
-  if (dc_dc_status.temp1 > 100.0 ||  dc_dc_status.temp2 > 100.0) {
+  if (dc_dc_status.temp1 > 10000 ||  dc_dc_status.temp2 > 10000) {
     fanEnabled = true;
   }
-  else if ( dc_dc_status.temp1 < 90 &&  dc_dc_status.temp2 < 90.0) {
+  else if ( dc_dc_status.temp1 < 9000 &&  dc_dc_status.temp2 < 9000) {
     fanEnabled = false;
   }
   
@@ -149,23 +168,25 @@ void fanHandler() {
 }
 
 void sendData() 
-{
+{  
   // Write the header
-  byte header[] = {0xAA, 0xBB, 0xCC, 0xDD };
+  byte header[] = { 0xAA, 0xBB, 0xCC, 0xDD };
   Serial.write(header, sizeof(header)); 
+
+  // Write the dc-dc status out
   Serial.write((byte *)&dc_dc_status, sizeof(dc_dc_status));  
   Serial.write((byte *)&controller_status, sizeof(controller_status));   
 }
 
 void setup()
 {
-    //Serial
+    // Serial
     Serial2.begin(115200);
-    Serial.begin(115200);
+    Serial.begin(19200);
   
     // Clear the data
-    memset(&dc_dc_status, 0, sizeof(DCDCStatus));
-    memset(&controller_status, 0, sizeof(ControllerStatus));
+    memset(&dc_dc_status, 0x00, sizeof(DCDCStatus));
+    memset(&controller_status, 0x00, sizeof(ControllerStatus));
 
     // Set the fan state
     pinMode(fanPin, OUTPUT);
@@ -180,9 +201,9 @@ void setup()
 
 void loop()
 {
+  sendData();
   fanHandler();
   dcDcCheckStatus(); 
   controllerCheckStatus();
-  sendData();
 }
 
